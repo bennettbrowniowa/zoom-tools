5c0b6f42bc841d8ac103ba9a729d9e5515db5535
Bennett Brown <bennettbrowniowa@users.noreply.github.com>
2021-11-27 18:38:32 -0600

    gen signature - zoom web mtg sdk
    as guide at
    https://marketplace.zoom.us/docs/sdk/native-sdks/web/signature

---
+++ zoom web mtg sdk.py
@@ -0,0 +1,25 @@
+import hashlib
+import hmac
+import base64
+import time
+
+def generateSignature(data):
+    ts = int(round(time.time() * 1000)) - 30000;
+    msg = data['apiKey'] + str(data['meetingNumber']) + str(ts) + str(data['role']);
+    message = base64.b64encode(bytes(msg, 'utf-8'));
+    # message = message.decode("utf-8");
+    secret = bytes(data['apiSecret'], 'utf-8')
+    hash = hmac.new(secret, message, hashlib.sha256);
+    hash =  base64.b64encode(hash.digest());
+    hash = hash.decode("utf-8");
+    tmpString = "%s.%s.%s.%s.%s" % (data['apiKey'], str(data['meetingNumber']), str(ts), str(data['role']), hash);
+    signature = base64.b64encode(bytes(tmpString, "utf-8"));
+    signature = signature.decode("utf-8");
+    return signature.rstrip("=");
+
+
+data = {'apiKey': "" ,
+            'apiSecret': "",
+            'meetingNumber': 888,
+            'role': 0}
+print (generateSignature(data))
