30866eb4bb924fed3e28944ca8b271a2363f0152
Bennett Brown <bennettbrowniowa@users.noreply.github.com>
2021-12-04 12:57:37 -0600

    ineffectual fix onload
    error in console getAllAttendeesList method doesn't exist. client was being assigned but only acquires method after line 48 is executed?? exists in console CLI

--- zoomTeacherBot.js
+++ zoomTeacherBot.js
@@ -1,5 +1,5 @@
 
-//get API keys
+//API keys are stored in a previous script in global class1
 
 console.log("Signature: ", class1.signature);
 userName = "Minion";
@@ -8,99 +8,103 @@ classToJoin = class1; //{name:,password:,meetingNumber:}
 // Import SDK
 // Following developer docs at
 // https://marketplace.zoom.us/docs/sdk/native-sdks/web/component-view/import-sdk
-
 const client = ZoomMtgEmbedded.createClient();
-let meetingSDKElement = document.getElementById('meetingSDKElement');
-client.init({
-  debug: true,
-  zoomAppRoot: meetingSDKElement,
-  language: 'en-US',
-  customize: {
-    meetingInfo: ['topic', 'host', 'mn', 'pwd', 'telPwd', 'invite', 'participant', 'dc', 'enctype'],
-    toolbar: {
-      buttons: [
-        {
-          text: 'Custom Button',
-          className: 'CustomButton',
-          onClick: () => {
-            console.log('custom button');
-          }
+window.onload = function() {
+    let meetingSDKElement = document.getElementById('meetingSDKElement');
+    client.init({
+      debug: true,
+      zoomAppRoot: meetingSDKElement,
+      language: 'en-US',
+      customize: {
+        meetingInfo: ['topic', 'host', 'mn', 'pwd', 'telPwd', 'invite', 'participant', 'dc', 'enctype'],
+        toolbar: {
+          buttons: [
+            {
+              text: 'Custom Button',
+              className: 'CustomButton',
+              onClick: () => {
+                console.log('custom button');
+              }
+            }
+          ]
         }
-      ]
-    }
-  }
-});
+      }
+    });
 
-// Join meeting
-// Following dev docs at
-// https://marketplace.zoom.us/docs/sdk/native-sdks/web/component-view/meetings
+    // Join meeting
+    // Following dev docs at
+    // https://marketplace.zoom.us/docs/sdk/native-sdks/web/component-view/meetings
 
 
-client.join({
-	apiKey: apiKey,
-	signature: classToJoin.signature,
-	meetingNumber: classToJoin.meetingNumber,
-	password: classToJoin.password,
-	userName: userName
-})
-/*
-//https://marketplace.zoom.us/docs/sdk/video/web/essential/chat
-//get chat interface object
-const chat = client.getChatClient();
-chat.sendToAll('hello everyone');
+    client.join({
+        apiKey: apiKey,
+        signature: classToJoin.signature,
+        meetingNumber: classToJoin.meetingNumber,
+        password: classToJoin.password,
+        userName: userName
+    })
 
-// send message to everyone
-chat.send('hello', userId)
+    //List participants
+    var participants = client.getAttendeesList();
+    getElementById("participants").innerHTML = ""
+    participants.foreach(function(s) {getElementById("participants").innerHTML += s});
 
-// send message to someone
-client.on('chat-on-message', (payload) => {
-  const {
-    message,
-    sender: {
-      name: senderName,
-      userIdSender,
-    },
-    receiver: {
-      name: receiverName,
-      userIdReceiver,
-    },
-    timestamp
-  } = payload;
-  console.log(`Message: ${message}, from ${senderName} to ${receiverName}`)
-})
+    //https://marketplace.zoom.us/docs/sdk/video/web/essential/chat
+    //get chat interface object
+    var chat = client.getChatClient();
+    chat.sendToAll('hello everyone');
 
-//send a chat to a user
+    // send message to everyone
+    chat.send('hello', userId)
 
-var UserIdStudent = userIdReceiver
-chat.send('hello', userIdStudent).then(payload => {
-  const {
-    message,
-    receiver: {
-      name,
-      userIdSender,
-    }
-  } = payload;
-  console.log(`Message: ${message}, from me to ${name}`)
-})
+    // send message to someone
+    client.on('chat-on-message', (payload) => {
+      const {
+        message,
+        sender: {
+          name: senderName,
+          userIdSender,
+        },
+        receiver: {
+          name: receiverName,
+          userIdReceiver,
+        },
+        timestamp
+      } = payload;
+      console.log(`Message: ${message}, from ${senderName} to ${receiverName}`)
+    })
 
-//send chat to everyone
-chat.sendToAll('hello everyone');
+    //send a chat to a user
 
-//receive messages
-client.on('chat-on-message', (payload) => {
-  const {
-    message,
-    sender: {
-      name: senderName,
-      userId,
-    },
-    receiver: {
-      name: receiverName,
-      userId,
-    },
-    timestamp
-  } = payload;
-  console.log(`Message: ${message}, from ${senderName} to ${receiverName}`)
-})
+    var UserIdStudent = userIdReceiver
+    chat.send('hello', userIdStudent).then(payload => {
+      const {
+        message,
+        receiver: {
+          name,
+          userIdSender,
+        }
+      } = payload;
+      console.log(`Message: ${message}, from me to ${name}`)
+    })
 
-*/
+    //send chat to everyone
+    chat.sendToAll('hello everyone');
+
+    //receive messages
+    client.on('chat-on-message', (payload) => {
+      const {
+        message,
+        sender: {
+          name: senderName,
+          userIdFrom,
+        },
+        receiver: {
+          name: receiverName,
+          userIdTo,
+        },
+        timestamp
+      } = payload;
+      console.log(`Message: ${message}, from ${senderName} to ${receiverName}`)
+    })
+}
