26871d3fec307c9da5fab42b46a17e765eee158e
Bennett Brown <bennettbrowniowa@users.noreply.github.com>
2021-12-04 12:53:09 -0600

    Fix zoom signatures autogeneration
    needed newlines
    made single variable host =0/1

--- zoom web mtg sdk.py
+++ zoom web mtg sdk.py
@@ -2,7 +2,7 @@ import hashlib
 import hmac
 import base64
 import time
-
+host = 1 #change to 0 for non-host, 1 for host
 def generateSignature(data):
     ts = int(round(time.time() * 1000)) - 30000;
     msg = data['apiKey'] + str(data['meetingNumber']) + str(ts) + str(data['role']);
@@ -23,28 +23,28 @@ def parse_meeting_link(meeting_link):
     return meeting_number, password
 
 def header_of_js(jsfile):
-    jsfile.write('// This file created by Python.')
-    jsfile.write('// Change content by changing the Python input file.')
-    jsfile.write('')
-    jsfile.write('var apiKey= "'+apiKey+'";')
-    jsfile.write('')
-    jsfile.write('var classes = [];')
-    jsfile.write('')
+    jsfile.write('// This file created by Python.\n')
+    jsfile.write('// Change content by changing the Python input file.\n')
+    jsfile.write('\n')
+    jsfile.write('var apiKey= "'+apiKey+'";\n')
+    jsfile.write('\n')
+    jsfile.write('var classes = [];\n')
+    jsfile.write('\n')
 
 def js_for_one_class(jsfile):
-    jsfile.write('var class1 = Object();')
-    jsfile.write('class1["name"] = "' + class_name + '";')
-    jsfile.write('class1["password"] = "' + meeting_password + '";')
-    jsfile.write('class1["meetingNumber"] = "' + str(meeting_number) + '";')
-    jsfile.write('class1["signature"] = "' + signature + '";')
-    jsfile.write('classes.push(class1);')
-    jsfile.write('')
+    jsfile.write('var class1 = Object();\n')
+    jsfile.write('class1["name"] = "' + class_name + '";\n')
+    jsfile.write('class1["password"] = "' + meeting_password + '";\n')
+    jsfile.write('class1["meetingNumber"] = "' + str(meeting_number) + '";\n')
+    jsfile.write('class1["signature"] = "' + signature + '";\n')
+    jsfile.write('classes.push(class1);\n')
+    jsfile.write('\n')
 
 # The API key and API secret key are specific to your zoom user,
 # and created by activating a free zoom dev account.
 # To keep them private from piblic git commits, they are in a separate gitignored file
 from zoom_links import apiKey, apiSecret, class_name_links
-data={'apiKey': apiKey , 'apiSecret': apiSecret, 'role': 0}
+data={'apiKey': apiKey , 'apiSecret': apiSecret, 'role': host}
 
 # Initialize outputs
 jsfile = open("zoom_api_keys.js","w")
